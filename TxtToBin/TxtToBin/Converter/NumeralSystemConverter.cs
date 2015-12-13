1726ba01b52d40473863f4a7a60a0ed1f05fc853
Igor Santarek <jegor377@gmail.com>
2015-12-13 01:50:54 +0100

    I figure out how to name functions in NumeralSystemConverter class. :)

--- TxtToBin/TxtToBin/Converter/NumeralSystemConverter.cs
+++ TxtToBin/TxtToBin/Converter/NumeralSystemConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace TxtToBin.Converter
 {
@@ -10,10 +9,10 @@ namespace TxtToBin.Converter
 
         public virtual string ConvertToNumbers(string valueToConvert)
         {
-            valueToConvert = removeStringEndingChars(valueToConvert);
+            valueToConvert = removeUnnecessaryCharacters(valueToConvert);
             if (!isValueEmpty(valueToConvert))
             {
-                var binaryResult = changeValueToNumeralSystem(valueToConvert);
+                var binaryResult = changeAllCharactersToNumeralSystem(valueToConvert);
                 return binaryResult;
             }
             return "";
@@ -21,47 +20,46 @@ namespace TxtToBin.Converter
 
         public virtual string ConvertFromNumbers(string valueToConvert)
         {
-            valueToConvert = removeStringEndingChars(valueToConvert);
+            valueToConvert = removeUnnecessaryCharacters(valueToConvert);
             if (!isValueEmpty(valueToConvert))
             {
-                var convertedTextResult = changeNumeralSystemToValue(valueToConvert);
+                var convertedTextResult = collectAllNumbersAndChangeThemToCharacters(valueToConvert);
                 return convertedTextResult;
             }
             return "";
         }
 
-        protected string changeValueToNumeralSystem(string valueToChange)
+        protected string changeAllCharactersToNumeralSystem(string valueToChange)
         {
             var numeralSystemResult = "";
-            var numeralSystemValueConvertedToASCIIbytes = Encoding.ASCII.GetBytes(valueToChange);
 
-            for (var i = 0; i < numeralSystemValueConvertedToASCIIbytes.Length; i++)
+            for (var i = 0; i < valueToChange.Length; i++)
             {
-                var convertedToNumeralSystemValue = convertToNumeralSystem(numeralSystemValueConvertedToASCIIbytes[i]);
+                var convertedToNumeralSystemValue = convertCharacterToNumeralSystemNumber((byte)valueToChange[i]);
 
-                // It adds some zeros on the front of the string (if the converted value is 
-                // less than format character count) to get static format.
+                // It adds some zeros on the front of the string (if the converted value length is 
+                // less than required length for this numeral system) to get static format.
                 numeralSystemResult += (convertedToNumeralSystemValue.Length == (int)characterCountForNumeralSystem ?
                     convertedToNumeralSystemValue :
-                    addZerosOnTheFrontOfResult(convertedToNumeralSystemValue));
+                    addZerosOnTheFrontOfNumeralSystemNumber(convertedToNumeralSystemValue));
             }
 
             return numeralSystemResult;
         }
 
-        protected string changeNumeralSystemToValue(string valueToChange)
+        protected string collectAllNumbersAndChangeThemToCharacters(string valueToChange)
         {
             var convertedTextResult = "";
 
-            // It goes through every character and if 'i' "is every second", then it sets 'i' to the sign.
+            // It goes through every characters and if 'i' "is every second number", then it sets 'i' to the sign.
             // Then it cuts two characters from the string (from 'j' to 'i') and adds the cut out 
             // value to convertedTextResult as char.
             // When it finish, it sets 'j' to 'i' to cut the next value from the last end point of cut.
             for (int i = 0, j = 0; i <= valueToChange.Length; i++)
             {
-                if (isEverySecond(i))
+                if (isNumberEverySecond(i))
                 {
-                    var convertedFromNumeralSystemValue = convertFromNumeralSystemToValue(valueToChange.Substring(j, i - j));
+                    var convertedFromNumeralSystemValue = convertNumberToCharacter(valueToChange.Substring(j, i - j));
                     convertedTextResult += Convert.ToChar(convertedFromNumeralSystemValue);
                     j = i;
                 }
@@ -70,22 +68,22 @@ namespace TxtToBin.Converter
             return convertedTextResult;
         }
 
-        protected string convertToNumeralSystem(byte valueToConvert)
+        protected string convertCharacterToNumeralSystemNumber(byte valueToConvert)
         {
             return Convert.ToString(valueToConvert, (int)numeralSystemBase);
         }
 
-        protected int convertFromNumeralSystemToValue(string valueToConvert)
+        protected int convertNumberToCharacter(string valueToConvert)
         {
-            return valueToConvert.Equals(string.Empty) ? 0 : Convert.ToInt32(valueToConvert, (int)numeralSystemBase);
+            return isValueEmpty(valueToConvert) ? 0 : Convert.ToInt32(valueToConvert, (int)numeralSystemBase);
         }
 
-        protected string addZerosOnTheFrontOfResult(string valueToAddZeros)
+        protected string addZerosOnTheFrontOfNumeralSystemNumber(string valueToAddZeros)
         {
             return new string('0', Math.Abs(valueToAddZeros.Length - (int)characterCountForNumeralSystem)) + valueToAddZeros;
         }
 
-        protected string removeStringEndingChars(string value)
+        protected string removeUnnecessaryCharacters(string value)
         {
             return value.Replace("\n", string.Empty).Replace("\r", string.Empty);
         }
@@ -95,7 +93,7 @@ namespace TxtToBin.Converter
             return valueToCheck.Equals(string.Empty);
         }
 
-        protected bool isEverySecond(int index)
+        protected bool isNumberEverySecond(int index)
         {
             return index % (int)characterCountForNumeralSystem == 0 && index != 0;
         }
